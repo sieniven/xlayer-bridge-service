bbc9aa0ac7b21191cb3a223a47c68d58a8b45b07
Vui-Chee <vuicheesiew@gmail.com>
2025-04-24 11:35:06 +0800

    Omit unused metrics cfg field

--- config/xlayer_config.go
+++ config/xlayer_config.go
@@ -25,8 +25,6 @@ type serverCfg struct {
 }
 
 type metricsCfg struct {
-	// Endpoint is the metrics endpoint for prometheus to query the metrics
-	Endpoint string `mapstructure:"Endpoint"`
 	// Env is the environment label for the metrics, to separate mainnet and testnet metrics
 	Env string `mapstructure:"Env"`
 }
