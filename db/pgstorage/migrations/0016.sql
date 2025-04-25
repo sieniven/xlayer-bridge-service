a5320478e6b586035576148f8d8a2073bfc95884
Vui-Chee <vuicheesiew@gmail.com>
2025-04-25 10:57:51 +0800

    Do not drop `received_at` column

--- db/pgstorage/migrations/0016.sql
+++ db/pgstorage/migrations/0016.sql
@@ -1,7 +1,9 @@
 -- +migrate Up
 ALTER TABLE sync.block DROP COLUMN IF EXISTS parent_hash;
-ALTER TABLE sync.block DROP COLUMN IF EXISTS received_at;
+
+-- XLayer still requires this field, so do not drop.
+-- ALTER TABLE sync.block DROP COLUMN IF EXISTS received_at;
 
 -- +migrate Down
 ALTER TABLE sync.block ADD COLUMN IF NOT EXISTS parent_hash BYTEA DEFAULT '\x0000000000000000000000000000000000000000000000000000000000000000';
-ALTER TABLE sync.block ADD COLUMN IF NOT EXISTS received_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT to_timestamp(0);
+-- ALTER TABLE sync.block ADD COLUMN IF NOT EXISTS received_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT to_timestamp(0);
--- db/pgstorage/migrations/1005.sql
+++
@@ -1,9 +0,0 @@
--- In RC14 onwards, the upstream version drops the DB column
--- from sync.block table. However, our XLayer code
--- still references this value.
-
--- +migrate Down
-ALTER TABLE sync.block DROP COLUMN IF EXISTS received_at;
-
--- +migrate Up
-ALTER TABLE sync.block ADD COLUMN IF NOT EXISTS received_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW();
