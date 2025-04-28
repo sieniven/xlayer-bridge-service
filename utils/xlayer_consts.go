a6cd0096504492af98bcd47585164d144a9c4fac
Vui-Chee <vuicheesiew@gmail.com>
2025-04-28 13:40:50 +0800

    minus one file

--- utils/xlayer_consts.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-const (
-	CtxTraceID string = "traceID"
-	TraceID           = "traceID"
-	traceIDLen        = 16
-)
--- utils/xlayer_helpers.go
+++ utils/xlayer_helpers.go
@@ -1,5 +1,11 @@
 package utils
 
+const (
+	CtxTraceID string = "traceID"
+	TraceID           = "traceID"
+	traceIDLen        = 16
+)
+
 // GenerateTraceID generates a random trace ID.
 func GenerateTraceID() string {
 	return generateRandomString(traceIDLen)
