c0f15bf62461e777e0c9768c4307ec856c764912
Vui-Chee <vuicheesiew@gmail.com>
2025-04-29 14:13:54 +0800

    Specify strict endpoint for bridge API

--- server/server.go
+++ server/server.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"time"
 
 	"github.com/0xPolygonHermez/zkevm-bridge-service/bridgectrl/pb"
@@ -17,6 +16,10 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/protobuf/encoding/protojson"
+)
+
+const (
+	bridgeEndpointPath = "/priapi/v1/ob/bridge"
 )
 
 // RunServer runs gRPC server and HTTP gateway
@@ -95,10 +98,15 @@ func runGRPCServer(ctx context.Context, bridgeServer pb.BridgeServiceServer, por
 }
 
 func preflightHandler(w http.ResponseWriter, r *http.Request) {
-	headers := []string{"Content-Type", "Accept"}
-	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
-	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
-	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	// headers := []string{"Content-Type", "Accept"}
+	// w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	// methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	// w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+
+	// XLayer
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 }
 
 // allowCORS allows Cross Origin Resource Sharing from any origin.
@@ -139,6 +147,11 @@ func runRestServer(ctx context.Context, grpcPort, httpPort string) error {
 	})
 	mux := runtime.NewServeMux(muxJSONOpt, muxHealthOpt)
 
+	// XLayer
+	httpMux := http.NewServeMux()
+	httpMux.Handle(bridgeEndpointPath+"/", http.StripPrefix(bridgeEndpointPath, mux))
+	httpMux.Handle("/", mux)
+
 	if err := pb.RegisterBridgeServiceHandler(ctx, mux, conn); err != nil {
 		return err
 	}
@@ -146,7 +159,7 @@ func runRestServer(ctx context.Context, grpcPort, httpPort string) error {
 	srv := &http.Server{
 		ReadTimeout: 1 * time.Second, //nolint:mnd
 		Addr:        ":" + httpPort,
-		Handler:     allowCORS(mux),
+		Handler:     allowCORS(httpMux), // XLayer
 	}
 
 	c := make(chan os.Signal, 1)
